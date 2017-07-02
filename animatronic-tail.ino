4c91fec9d26ef6a005ffb96affcb42f348a30855
Zoe <github@z303.org>
2017-07-02 20:09:37 +0100

    Rename servo variable

--- animatronic-tail.ino
+++ animatronic-tail.ino
@@ -1,6 +1,6 @@
 #include <Servo.h>
 
-Servo myservo;
+Servo tailservo;
 
 int pos = 0 ;
 int final = 0;
@@ -9,7 +9,7 @@ int time = 0;
 
 void setup()
 {
-  myservo.attach(9);
+  tailservo.attach(9);
 }
 
 
@@ -19,25 +19,26 @@ void loop()
   final2 = random(10, 30);
   for (pos = 90; pos < final; pos += 1)
   {
-    myservo.write(pos);
+    tailservo.write(pos);
     delay(final / 15);
   }
   delay(final / 2);
   for (pos = final; pos >= 90; pos -= 1)
   {
-    myservo.write(pos);
+    tailservo.write(pos);
     delay(final / 30);
   }
 
   for (pos = 90; pos >= final2; pos -= 1)
   {
-    myservo.write(pos);
+    tailservo.write(pos);
     delay(final / 15);
   }
   delay(final / 2);
   for (pos = final2; pos < 90; pos += 1)
   {
-    myservo.write(pos);
+    tailservo.write(pos);
     delay(final / 30);
   }
 }
+
